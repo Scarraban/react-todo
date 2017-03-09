6f795ef308ccfe5d3117d52124462e38921f7f36
Sam Butler-Thompson <sam@q2q-it.com>
2017-03-09 20:47:16 +0000

    Fetch todos from firebase on app start

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -81,17 +81,37 @@ describe('Actions', () => {
     var testTodoRef;
 
     beforeEach((done) => {
-      testTodoRef = firebaseRef.child('todos').push();
+      var todosRef = firebaseRef.child('todos');
+      todosRef.remove().then(() => {
+        testTodoRef = firebaseRef.child('todos').push();
 
-      testTodoRef.set({
-        text: 'Something to do',
-        completed: false,
-        createdAt: 123456
-      }).then(() => done());
+        testTodoRef.set({
+          text: 'Something to do',
+          completed: false,
+          createdAt: 123456
+        })
+      })
+      .then(() => done())
+      .catch(done);;
     });
 
     afterEach((done) => {
       testTodoRef.remove().then(() => done());
+    });
+
+    it('should call startAddTodos and dispatch ADD_TODOS action', (done) => {
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+        expect(mockActions[0]).toInclude({
+          type: 'ADD_TODOS'
+        });
+        expect(mockActions[0].todos.length).toEqual(1);
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
+        done();
+      }, done);
     });
 
     it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
--- public/bundle.js
+++ public/bundle.js
@@ -28096,14 +28096,15 @@ var TodoList = React.createClass({
   render: function () {
     var { todos, showCompleted, searchText } = this.props;
     var renderTodos = () => {
-      if (todos.length == 0) {
+      var todosOutput = TodoAPI.filterTodos(todos, showCompleted, searchText);
+      if (todosOutput.length == 0) {
         return React.createElement(
           'p',
           { className: 'container__message' },
           'Nothing to do!'
         );
       }
-      return TodoAPI.filterTodos(todos, showCompleted, searchText).map(todo => {
+      return todosOutput.map(todo => {
         return React.createElement(__WEBPACK_IMPORTED_MODULE_0_Todo__["a" /* default */], _extends({ key: todo.id }, todo));
       });
     };
