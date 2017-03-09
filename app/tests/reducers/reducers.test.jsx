84e4a09efcab615232b80a9f69c2a9402e24babe
Sam Butler-Thompson <sam@q2q-it.com>
2017-03-09 14:33:34 +0000

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -24,4 +24,36 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('should toggle todo', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var todoArr = [{
+        id: 1,
+        text: 'Test Task',
+        completed: false,
+        createdAt: 123456,
+        completedAt: undefined
+      }];
+      var res = reducers.todosReducer(df(todoArr), df(action));
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toBeA('number');
+
+      res = reducers.todosReducer(df(res), df(action));
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
