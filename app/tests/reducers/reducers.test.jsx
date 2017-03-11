cee5d7c5f229716d77d2c027226dc564f5b9638a
Sam Butler Thompson <Sam Butler Thompson>
2017-03-11 21:04:14 +0000

    wipe todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -80,6 +80,22 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+
+    it('should remove todos on logout', () => {
+      var todos = [{
+        id: 1,
+        text: 'Test Task',
+        completed: false,
+        createdAt: 123456,
+        completedAt: undefined
+      }];
+      var action = {
+        type: 'LOGOUT'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(0);
+    });
   });
 
   describe('authReducer', () => {
